a0eefdaeab29dbb966eb4deba6c001707e7964c3
JTSenneker <JTSenneker@gmail.com>
2016-11-03 00:48:52 -0400

    Character Movement
    Programmed character movement using Unity's 2D physics engine

---
+++ UltraSuperHyperPuzzlePlatformerDeluxeTurboArcadeEditionEXPlusAlphaAndKnuckles/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10;
+    public float jumpHeight = 15;
+    public float groundDetectDistance = 1;
+
+    public LayerMask layerMask;
+    Rigidbody2D body;
+
+    bool canJump = true;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxisRaw("Vertical");
+        
+        if (moveV > 0 && canJump) {
+            canJump = false;
+            body.velocity += new Vector2(0, jumpHeight);
+        }
+        body.AddForce(Vector2.right * moveH * speed);
+        //Debug.DrawLine(transform.position, new Vector3(body.position.x, body.position.y * groundDetectDistance, 0));
+        if(Physics2D.Raycast(body.position, Vector2.down, groundDetectDistance, layerMask)) {
+            print("Grounded");
+            canJump = true;
+        }
+
+        
+	}
+}
